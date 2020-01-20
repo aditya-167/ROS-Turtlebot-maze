b85e5f1fdf8780d26904631b4facbef85da1b3e7
Aditya Srichandan <47297221+aditya-167@users.noreply.github.com>
2020-01-20 19:58:00 +0530

    Add files via upload

---
+++ src/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/user/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export GAZEBO_MODEL_PATH="/home/simulations/public_sim_ws/install/share/osrf_gear/models:$GAZEBO_MODEL_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/home/simulations/public_sim_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib"
+export PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/home/simulations/public_sim_ws/devel/bin:/opt/ros/kinetic/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+export PWD="/home/user/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/user/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/user/catkin_ws/src:$ROS_PACKAGE_PATH"
